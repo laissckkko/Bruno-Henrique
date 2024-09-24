06aadc9c517cf6d5bd35a9a51e6df0a3b1f255c0
laissckkko <Lais.sckoteski.araujo@escola.pr.gov.br>
2024-09-24 12:14:52 -0300

    s

--- main.js
+++ main.js
@@ -8,6 +8,6 @@ function soma (num1, num2){
         let areatrapezio = function(teste1, teste2, teste3){
     return (teste1+teste2*teste3/2)
         }
-        let nome=prompt(tigreza vip);
+        let nome=(prompt(tigreza vip));
         DocumentFragment.write (nome);
         parseInt(prompt(nome))
