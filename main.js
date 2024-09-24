0349abb636014bebf4c5835e04ce9a3e76d97cf2
laissckkko <Lais.sckoteski.araujo@escola.pr.gov.br>
2024-09-24 12:26:13 -0300

    s

--- main.js
+++ main.js
@@ -6,8 +6,9 @@ function soma (num1, num2){
         return lado**2;
     }
         let areatrapezio = function(teste1, teste2, teste3){
-    return (teste1+teste2*teste3/2)
-        }
-      let nome= prompt(trigresaip)
-      document.write(trigresavip)
-      parseInt(prompt(tigesavip))
+    return (teste1+teste2*teste3/2)}
+    let tigresavip=prompt(tigresavip);
+    document.write (tigresavip);
+    parseInt(prompt(tigresavip))
+
+    
