8e315ce5a7d33c23ea515708b3257ce0ac1cf298
laissckkko <Lais.sckoteski.araujo@escola.pr.gov.br>
2024-09-24 12:20:57 -0300

    a

--- main.js
+++ main.js
@@ -8,4 +8,6 @@ function soma (num1, num2){
         let areatrapezio = function(teste1, teste2, teste3){
     return (teste1+teste2*teste3/2)
         }
-      
+      let nome= prompt(trigresaip)
+      document.write(trigresavip)
+      parseInt(prompt(tigesavip))
