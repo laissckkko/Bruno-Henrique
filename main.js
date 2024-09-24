19ee5eb6dcf4f43700fb76cd037cf95c32fe1ef7
laissckkko <Lais.sckoteski.araujo@escola.pr.gov.br>
2024-09-24 11:58:35 -0300

    ç

--- main.js
+++ main.js
@@ -4,5 +4,7 @@ function soma (num1, num2){
 }
     function areaQuadrado (lado){
         return lado**2;
-
-}
+    }
+        let areatrapezio = function(teste1, teste2, teste3){
+    return teste1+teste2*teste3/2
+        }
